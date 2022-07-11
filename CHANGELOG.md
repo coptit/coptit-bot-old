fc1e048f1e2df989874b45f216e44e417a93adc1
Kunal Singh <kunalsin9h@gmail.com>
2022-07-11 23:24:51 +0530

    feat: welcome message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,9 +7,19 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.0] - 2022-07-11
+### Added
+- On joining of a new user it can give a welcome message in a particular channel. (issue: [#5](https://github.com/KunalSin9h/coptit-bot/issues/5))
+
+### Fixed
+- Message on command was not working. (issue: [#2](https://github.com/KunalSin9h/coptit-bot/issues/2))
+
+
+
 ## [1.0.0] - 2022-07-08
 ### Added
 - Can send normal, embed, normal + embed messages to any channel without command.
 
 [Unreleased]: https://github.com/KunalSin9h/coptit-bot/compare/v1.0.0...HEAD
+[1.1.0]: https://github.com/KunalSin9h/coptit-bot/compare/v1.0.0...v1.1.0
 [1.0.0]: https://github.com/KunalSin9h/coptit-bot/releases/tag/v1.0.0
--- src/main.py
+++ src/main.py
@@ -19,67 +19,83 @@ remember time stamp should be like this.
 "timestamp": "2021-12-09T11:36:00.000+00:00",
 """
 
-client = discord.Client()
+# Enable all Intents
+intents = discord.Intents.all()
+intents.members = True
+client = discord.Client(intents=intents)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
-
-print(BOT_TOKEN)
+MESSAGE_CHANNEL_ID = os.getenv("MESSAGE_CHANNEL_ID")
+WELCOME_CHANNEL_ID = os.getenv("WELCOME_CHANNEL_ID")
 
 @client.event
 async def on_ready():
-    # await client.change_presence(activity=discord.Game(name="with code!"))
+    #await client.change_presence(activity=discord.Game(name="with code!"))
     print(f"Logged in as {client.user} (ID: {client.user.id})")
 
+@client.event
+async def on_member_join(member):
+    welcome_msg = "Hey, <@" + str(member.id) + "> welcome to **Server!**, pick your #roles and do write suggestion in #suggestion-box if you have any!"
+    channel = await client.fetch_channel(WELCOME_CHANNEL_ID)
+    await channel.send(welcome_msg)
+
 # content message(normal message)
-content_file = open("../content.txt", "r+")
+content_file = open("./content.txt", "r+")
 content_msg = content_file.read()
 
 # embed message
-embed_file = open("../embed.json", "r+")
+embed_file = open("./embed.json", "r+")
 
-if_message_sended = False
+if_auto_message_sended = False
 
 # this code is used when send embed message without command
 @tasks.loop(seconds=60)
 async def message_send():
 
-    global if_message_sended
+    global if_auto_message_sended
 
-    if if_message_sended == False:
+    if if_auto_message_sended == False:
 
-        channel = await client.fetch_channel(CHANNEL_ID)  # find channel to send message
+        channel = await client.fetch_channel(MESSAGE_CHANNEL_ID)  # find channel to send message
 
         # if embed.json is not empty then send this message
         if os.stat("embed.json").st_size != 0:
             embed = discord.Embed.from_dict(json.load(embed_file))
             await channel.send(content=content_msg, embed=embed)
-            print("Message send successfully !")
+
+            # clear both message files
+            content_file.truncate(0)
+            embed_file.truncate(0)
+
         elif os.stat("content.txt").st_size != 0:
             await channel.send(content=content_msg)
-            print("Message send successfully !")
 
-        # clear both message files
-        content_file.truncate(0)
-        embed_file.truncate(0)
-
-        if_message_sended = True
+            content_file.truncate(0)
 
 
-# either above will work or bellow both will not work together.
+        if_auto_message_sended = True
 
 # This code is used when we have to give command and then bot will send embed message
-"""
 @client.event
 async def on_message(message):
-
     if message.author == client.user:
         return
+    if message.content.startswith("^send"):
 
-    if message.content.startswith('^send_msg'):
-        embed = discord.Embed.from_dict(json.load(embed_file))
-        await message.channel.send(content=content_msg, embed=embed)
-"""
+        # if embed.json is not empty then send this message
+        if os.stat("embed.json").st_size != 0:
+            embed = discord.Embed.from_dict(json.load(embed_file))
+            await message.channel.send(content=content_msg, embed=embed)
+
+            # clear both message files
+            content_file.truncate(0)
+            embed_file.truncate(0)
+
+        elif os.stat("content.txt").st_size != 0:
+            await message.channel.send(content=content_msg)
+
+            content_file.truncate(0)
+
 
 message_send.start()
 
